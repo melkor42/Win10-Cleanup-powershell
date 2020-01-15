9dab984bd555c6e6e1768edc9f50a35f8f5d4ac5
Manuel <manuel.spaeth@nttdata.com>
2020-01-15 14:30:40 +0100

    added git aliases

--- 03x_Choco_IDE.ps1
+++ 03x_Choco_IDE.ps1
@@ -12,4 +12,7 @@ choco install jetbrainstoolbox -y
 choco install rufus -y
 #choco install virtualbox -y
 echo "install intellij over the toolbar -> easy way to keep Updates"
+echo plugins Material ui
+echo settings terminal -> "C:\Program Files\Git\bin\sh.exe" -li  as "Shell Path"
+echo save .bash_profile -> %USERPROFILE%
 pause
